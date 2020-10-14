2ef64161b5cec0291e00440f6eb22891b13cdb9d
Bec Sun <rebeccasun86@gmail.com>
2020-10-14 17:02:20 +0100

    table added

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,10 @@ function init(){
   const blurb = document.querySelector('.container-2')
   const title = document.querySelector('.title')
 
+  const cells = []
+  const width = 10
+  const numberOfCells = width * width
+
   //change home page to game page
   function gamePage(){
     startButton.remove()
@@ -16,7 +20,12 @@ function init(){
   function startGame () {
     gamePage()  
 
-    
+    for (let i = 0; i < numberOfCells; i++ ){
+      const cell = document.createElement('div')
+      cells.push(cell)
+      grid.appendChild(cell)
+      console.log() 
+    } 
   }
 
   startButton.addEventListener('click', startGame)
